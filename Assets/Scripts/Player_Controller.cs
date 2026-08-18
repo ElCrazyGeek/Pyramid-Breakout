79a597ec420001627e5fc5a0c44d863d554b5370
ElCrazyGeek <elcrazygeeksoyyo@gmail.com>
2026-08-18 16:53:52 -0600

    movimiento

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -1,18 +1,34 @@
 using UnityEngine;
-using UnityEngine.Rendering;
+using UnityEngine.InputSystem;
 
 public class Player_Controller : MonoBehaviour
 {
     public float speed =15.0f;
     Vector2 moveLimits = new Vector2(8f, 4.5f);
 
-    public Vector2 currentInput;
-    
+    public Vector2 PlayerInput;
+
+    public void OnMovimiento(InputValue value)
+    {
+        PlayerInput = value.Get<Vector2>();
+    }
+
 
     void Update()
     {
-        GetInput();
-        Mover();
+        mover();
+
+    }
+
+
+    void mover() { 
+        Vector3 newPosition = transform.localPosition + 
+        (Vector3)PlayerInput * speed * Time.deltaTime;
+
+        newPosition.x = Mathf.Clamp(newPosition.x, -moveLimits.x, moveLimits.x);
+        newPosition.y = Mathf.Clamp(newPosition.y, -moveLimits.y, moveLimits.y);
+
+        transform.localPosition = newPosition;
 
     }
 }
