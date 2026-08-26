5b54a5dc213c72c2b10614e36ac098b550e4d619
ElCrazyGeek <elcrazygeeksoyyo@gmail.com>
2026-08-25 18:11:03 -0600

    cambios pequeños

--- Assets/Scripts/Cambiar camara.cs
+++ Assets/Scripts/Cambiar camara.cs
@@ -1,21 +1,36 @@
 using UnityEngine;
 using Unity.Cinemachine;
+using System;
+
 
 public class Cambiarcamara : MonoBehaviour
 {
-  
+
     [SerializeField] public CinemachineCamera camaraRiel;
     [SerializeField] public CinemachineCamera camaraLibre;
+    [SerializeField] public GameObject MirillaRiel;
 
+    [SerializeField] public GameObject MirillaLibre;
+
+    public Boolean modoLibre;
+
+    /*private void Update()
+    {
+        if (modoLibre != true) { 
+            Mirilla = MirillaRiel;
+        }
+    }*/ 
     public void ActivarModoLibre()
     {
         camaraLibre.Priority = 20;
         camaraRiel.Priority = 10;
+        modoLibre = true;
     }
 
     public void ActivarModoRiel()
     {
         camaraRiel.Priority = 20;
         camaraLibre.Priority = 10;
+        modoLibre = false;
     }
 }
